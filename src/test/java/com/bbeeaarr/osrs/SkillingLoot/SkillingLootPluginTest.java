d6b8155506f911e32f3862705f4bfa506ba6832e
Ourmond <armandrv@uci.edu>
2026-04-04 12:14:16 -0700

    initial commit

--- src/test/java/com/bbeeaarr/osrs/SkillingLoot/SkillingLootPluginTest.java
+++ src/test/java/com/bbeeaarr/osrs/SkillingLoot/SkillingLootPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.bbeeaarr.osrs.SkillingLoot;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class SkillingLootPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(SkillingLootPlugin.class);
 		RuneLite.main(args);
 	}
 }
