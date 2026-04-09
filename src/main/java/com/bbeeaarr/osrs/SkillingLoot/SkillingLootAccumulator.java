000ce1f4955567cfb25fa83169502a04527fc3bc
Ourmond <armandrv@uci.edu>
2026-04-08 21:29:58 -0700

    add loot accumulator

---
+++ src/main/java/com/bbeeaarr/osrs/SkillingLoot/SkillingLootAccumulator.java
@@ -0,0 +1,61 @@
+package com.bbeeaarr.osrs.SkillingLoot;
+
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.loottracker.PluginLootReceived;
+import net.runelite.http.api.loottracker.LootRecordType;
+import net.runelite.client.game.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SkillingLootAccumulator {
+    private final Plugin source;
+    private final String name;
+    private int amount = 1;
+    private Object metadata;
+
+    private ItemStack base;
+    private int baseMultiplier = 1;
+    private final List<ItemStack> extras = new ArrayList<>();
+
+    public SkillingLootAccumulator(Plugin source, String name, ItemStack baseItem)
+    {
+        this.name = name;
+        this.base = baseItem;
+        this.source = source;
+    }
+
+    public void addBonusBaseItem()
+    {
+        if (base == null)
+            throw new IllegalStateException("No base item set");
+        baseMultiplier++;
+    }
+
+    public void addBonusOtherItem(ItemStack bonus)
+    {
+        extras.add(bonus);
+    }
+
+    public PluginLootReceived build()
+    {
+        if (base == null)
+            throw new IllegalStateException("Cannot build: base item not set");
+        return PluginLootReceived.builder()
+                .source(source)
+                .name(name)
+                .type(LootRecordType.EVENT)
+                .items(buildFinalItemStack())
+                .build();
+
+    }
+
+    private List<ItemStack> buildFinalItemStack()
+    {
+        final List<ItemStack> allItems = new ArrayList<>();
+        allItems.add(new ItemStack(base.getId(), base.getQuantity() * baseMultiplier));
+        extras.forEach(item -> allItems.add(new ItemStack(item.getId(), item.getQuantity())));
+        return allItems;
+    }
+}
